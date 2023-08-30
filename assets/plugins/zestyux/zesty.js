cac907db889d108d081617ca61350d24bf7cf767
Rodney Gauna <rodneygauna@gmail.com>
2023-08-30 10:34:01 -0700

    better darkmode toggle logic with mustache

--- assets/plugins/zestyux/zesty.js
+++ assets/plugins/zestyux/zesty.js
@@ -16,10 +16,20 @@
 --------------------------- */
 const userHasDarkMode = localStorage.getItem('dark-mode') === 'enabled';
 
+// Check if user has dark mode enabled
 if (userHasDarkMode) {
     document.body.classList.add('dark-mode-variables');
-    darkMode.querySelector('span:nth-child(1)').classList.remove('active');
-    darkMode.querySelector('span:nth-child(2)').classList.add('active');
+    setTimeout(function () {
+        const darkMode = document.querySelector('.dark-mode');
+        darkMode.querySelector('span:nth-child(1)').classList.remove('active');
+        darkMode.querySelector('span:nth-child(2)').classList.add('active');
+    }, 40);
+} else {
+    setTimeout(function () {
+        const darkMode = document.querySelector('.dark-mode');
+        darkMode.querySelector('span:nth-child(1)').classList.add('active');
+        darkMode.querySelector('span:nth-child(2)').classList.remove('active');
+    }, 40);
 }
 
 function setDarkMode() {
